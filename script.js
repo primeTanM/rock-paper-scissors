a8b5648417ea9641032fad643bf9857dcef84957
Tanmay <tanmaymisra619@gmail.com>
2021-08-18 16:35:45 +0530

    finished project

--- script.js
+++ script.js
@@ -2,23 +2,58 @@ let picks = document.querySelectorAll("#picks");
 let stepOne = document.querySelector('.game-container-step-1');
 let stepTwo = document.querySelector('.game-container-step-2');
 let result = document.querySelector('.result-text');
+let score = document.querySelector('.score-number');
+let turnBorder = document.querySelector('.turn-border');
+let yourPickBorder = document.getElementById("your-pick-border");
+let yourPickTurn = document.getElementById("your-pick-turn");
+let housePickBorder = document.getElementById("house-pick-border");
+let housePickTurn = document.getElementById("house-pick-turn");
+let playAgain = document.querySelector('.play-again');
+
 let yourPick;
 let randomNumber;
 let housePick;
+let count = 0;
+
 
 picks.forEach(btn => {
     btn.addEventListener("click", () => {
+        let first = setInterval(() => {
+            turnBorder.style.display="none";
+            housePickBorder.classList.remove("turn");
+            housePickBorder.classList.add("blank-turn");
+            
+            
+        },3000);
+
+        clearInterval(first);
+
         stepOne.style.display="none";
         stepTwo.style.display="flex";
         yourPick = (btn.className);
         console.log(yourPick);
+        yourPickBorder.classList.add(yourPick + '-border');
+        yourPickTurn.classList.add(yourPick.split('')[0]);
         decideWinnner();
-        
-        // stepTwo.style.display="flex";
-
-
+        housePickBorder.classList.add(housePick + '-border');
+        housePickTurn.classList.add(housePick.split('')[0]);
+        updateScore();
     })
 })
+
+playAgain.addEventListener('click', () => {
+    stepOne.style.display="grid";
+    yourPickBorder.classList.remove(yourPick + '-border');
+    yourPickTurn.classList.remove(yourPick.split('')[0]);
+    housePickBorder.classList.remove(housePick + '-border');
+    housePickTurn.classList.remove(housePick.split('')[0]);
+    yourPick = "";
+    housePick = "";
+    
+    console.log(yourPick);
+    console.log(housePick);
+})
+
 
 function decideWinnner(){
     randomNumber = Math.floor(Math.random() * 3);
@@ -60,5 +95,16 @@ function decideWinnner(){
 }
 
 function updateScore(){
+    
+    if (result.innerText == "YOU WIN"){
+        count += 1;
+    }
+
+    else if (result.innerText == "YOU LOSE"){
+        count -= 1;
+    }
+
+    score.innerText = count;
 
 }
+
